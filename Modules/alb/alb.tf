7a14ff2abc689903a315527422b45af6bdaa8037
Prakash0301 <prakash.s030194@gmail.com>
2025-03-26 18:36:57 +0530

    test

---
+++ Modules/alb/alb.tf
@@ -0,0 +1,11 @@
+resource "aws_lb" "test" {
+  name               = "test-lb-tf"
+  internal           = false
+  load_balancer_type = "application"
+
+  enable_deletion_protection = true
+
+  tags = {
+    Environment = "production"
+  }
+}
